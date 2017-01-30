4a69780b744f7e01873576816f4eda599d1b76fe
Jonathan Wohl <jon@jonwohl.com>
2017-01-30 11:20:18 -0500

    Use node 6.9.x

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ openframe_do_install() {
   . ~/.nvm/nvm.sh
   # install Node.js 4.3
   echo "install node"
-  nvm install 4.3
+  nvm install 6.9
 
   # disable terminal screen blanking
   openframe_edit_or_add ~/.bashrc "setterm -powersave off -blank 0"
